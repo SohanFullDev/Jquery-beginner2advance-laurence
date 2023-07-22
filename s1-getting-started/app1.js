c7701b01d86dc348318196886628a11b0fa50062
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-22 13:35:12 +0600

    2_selection of page elements & DOM elements selection jquery_s1_t4

--- s1-getting-started/app1.js
+++ s1-getting-started/app1.js
@@ -1,7 +1,18 @@
-//$('div').html('Programmer Sohan');
-/*$('div').on('click',()=>{
-    $('div').html('Ami Sohan');
-})*/
-$('div').on('click',()=>{
-    $('div').html('Ami Sohan');
-}).html('Click Me');
+
+console.log($('div'));
+$('div').html('Hello');
+console.log($('.div2'));
+console.log($('#div3'));
+console.log($('#div3, .div2'));
+
+
+const $ele1 = $('div');
+//console.log(ele1[0]);
+console.log($ele1);
+$ele1.html('Hello 2');
+const $ele2 = $('.div2');
+console.log($ele2);
+const $ele3 = $('#div3');
+console.log($ele3);
+const $ele4 = $('#div3, .div2');
+console.log($ele4);
